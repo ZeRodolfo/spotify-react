113a7ebd351d43241c05a31d781819560e3e814a
José Rodolfo <j.rodolfopps@gmail.com>
2019-06-28 19:23:46 -0300

    Criado o componente sidebar, onde irá conter os itens do nosso menu do spotify

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { Container, NewPlaylist, Nav } from "./styles";
+
+import AddPlaylistIcon from "../../assets/images/add_playlist.svg";
+
+const Sidebar = () => (
+  <Container>
+    <div>
+      <Nav bold>
+        <li>
+          <a href="">Navegar</a>
+        </li>
+        <li>
+          <a href="">Rádio</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>SUA BIBLIOTECA</span>
+        </li>
+        <li>
+          <a href="">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="">Tocados Recentemente</a>
+        </li>
+        <li>
+          <a href="">Músicas</a>
+        </li>
+        <li>
+          <a href="">Álbuns</a>
+        </li>
+        <li>
+          <a href="">Artístas</a>
+        </li>
+        <li>
+          <a href="">Estações</a>
+        </li>
+        <li>
+          <a href="">Arquivos Locais</a>
+        </li>
+        <li>
+          <a href="">Vídeos</a>
+        </li>
+        <li>
+          <a href="">Podcasts</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>Playlists</span>
+        </li>
+        <li>
+          <a href="">Melhores do Rock</a>
+        </li>
+        <li>
+          <a href="">Mixer</a>
+        </li>
+      </Nav>
+    </div>
+    <NewPlaylist>
+      <img src={AddPlaylistIcon} alt="Adicionar playlist" />
+      Nova Playlist
+    </NewPlaylist>
+  </Container>
+);
+
+export default Sidebar;
