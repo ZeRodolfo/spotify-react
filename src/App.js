dcd8dfb91de938f50fd4493d0c47f172d81cd684
José Rodolfo <j.rodolfopps@gmail.com>
2019-07-01 15:20:02 -0300

    Adicionado a configuração de desenvolvimento para o Reactotron

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import "./config/reactotron";
 
 import GlobalStyle from "./styles/global";
 import { Wrapper, Container, Content } from "./styles/components";
