17c2ecc98befa201f70f04a1173487b7a02aad4f
José Rodolfo <j.rodolfopps@gmail.com>
2019-07-01 09:15:11 -0300

    Criando o componente Player onde irá controlar a execução de nossas músicas no futuro e estilizando o mesmo.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Fragment } from "react";
 
 import GlobalStyle from "./styles/global";
-import Sidebar from './components/Sidebar'
+
+import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
+import { Wrapper, Container } from "./styles/components";
 
 const App = () => (
   <Fragment>
     <GlobalStyle />
-    <Sidebar/>
+    <Wrapper>
+      <Container>
+        <Sidebar />
+      </Container>
+      <Player />
+    </Wrapper>
   </Fragment>
 );
 
